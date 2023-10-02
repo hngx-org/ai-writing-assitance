9e9c75d9cacb5c7e2011adee83782741b09b50cf
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-10-02 06:34:39 +0100

    home_screen0.1

--- app/src/main/java/com/example/aiwritingassitance/screens/Data.kt
+++ app/src/main/java/com/example/aiwritingassitance/screens/Data.kt
@@ -2,7 +2,7 @@ package com.example.aiwritingassitance.screens
 
 object Data {
     var prompts = listOf<String>(
-        "Write a Gmail to my boss at work with the subject of Apology",
+        "Write a Gmail to my boss at work with a subject of Apology",
         "Write an article on Android Development with Kotlin",
         "Write an essay titled The Pros and Cons of Homeschooling.",
         "Write a Gmail on the subject Happy birthday to my friend"
