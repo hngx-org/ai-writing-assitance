8a47c103e7078cc5ba5d30caa2a0e34c7b47bce4
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-10-07 05:29:46 +0100

    loginauth complete

--- app/src/main/java/com/example/aiwritingassitance/MyApp.kt
+++ app/src/main/java/com/example/aiwritingassitance/MyApp.kt
@@ -1,4 +1,17 @@
 package com.example.aiwritingassitance
 
-class MyApp {
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+class MyApp: Application() {
+    companion object {
+        lateinit var appContext: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = applicationContext
+
+    }
 }
